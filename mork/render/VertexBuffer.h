f3364a0f1e1678435120d64016255a0d01324a0f
LarsFlaeten <lars.flaeten@gmail.com>
2019-03-24 06:55:57 +0100

    Add TBN vertex definition

--- mork/render/VertexBuffer.h
+++ mork/render/VertexBuffer.h
@@ -75,6 +75,8 @@ struct vertex_pos_norm_uv {
     mork::vec2f     uv;
 
     vertex_pos_norm_uv(const mork::vec3f& p, const mork::vec3f& n, const mork::vec2f u) : pos(p), norm(n), uv(u) {};
+    vertex_pos_norm_uv() : pos(mork::vec3f::ZERO), norm(mork::vec3f::ZERO), uv(mork::vec2f::ZERO) {};
+
 
     inline static void setAttributes() {
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -85,6 +87,40 @@ struct vertex_pos_norm_uv {
         
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
         glEnableVertexAttribArray(2);
+    }
+};
+
+// Vertex object with normal and tangent/bitanget vector.
+struct vertex_pos_norm_tang_bitang_uv {
+    mork::vec3f     pos;
+    mork::vec3f     norm;
+    mork::vec3f     tang;
+    mork::vec3f     bitang;
+    mork::vec2f     uv;
+
+    vertex_pos_norm_tang_bitang_uv(const mork::vec3f& p, const mork::vec3f& n, const mork::vec3f& t, const mork::vec3f bt, const mork::vec2f u) : pos(p), norm(n), tang(t), bitang(bt), uv(u) {};
+
+    vertex_pos_norm_tang_bitang_uv() : pos(mork::vec3f::ZERO), norm(mork::vec3f::ZERO), tang(mork::vec3f::ZERO), bitang(mork::vec3f::ZERO), uv(mork::vec2f::ZERO) {};
+
+
+    inline static void setAttributes() {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3*sizeof(float)));
+        glEnableVertexAttribArray(1);
+        
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6*sizeof(float)));
+        glEnableVertexAttribArray(2);
+        
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(9*sizeof(float)));
+        glEnableVertexAttribArray(3);
+        
+        glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(12*sizeof(float)));
+        glEnableVertexAttribArray(4);
+
+
+
     }
 };
 
