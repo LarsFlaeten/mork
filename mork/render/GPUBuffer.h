398807ce0d9343db5af21394e571ebd136037e4d
LarsFlaeten <lars.flaeten@gmail.com>
2019-03-24 06:49:49 +0100

    Add buffer map and scoped map view

--- mork/render/GPUBuffer.h
+++ mork/render/GPUBuffer.h
@@ -6,9 +6,52 @@
 #include "mork/render/Bindable.h"
 #include "mork/glad/glad.h"
 #include "mork/ui/GlfwWindow.h"
-
+#include "mork/core/Log.h"
 
 namespace mork {
+    enum BufferAccess {
+        ReadOnly,
+        WriteOnly,
+        ReadWrite
+
+    };
+
+    // Scoped buffer view
+    template<typename T>
+    class BufferView {
+        public:
+        BufferView(T& buf, BufferAccess access) : t(buf) {
+            ptr = buf.mapBuffer(access);
+        }
+
+        ~BufferView() {
+            t.unmapBuffer();
+        }
+
+        void* get() {return ptr;}
+
+        private:
+            const T& t;
+            void* ptr;
+    };    
+     
+    // Scoped constant buffer view
+    template<typename T>
+    class ConstBufferView {
+        public:
+        ConstBufferView(const T& buf) : t(buf), ptr(buf.mapBuffer(BufferAccess::ReadOnly)) {}
+
+        ~ConstBufferView() {
+            t.unmapBuffer();
+        }
+
+        const void* get() {return ptr;}
+
+        private:
+            const T& t;
+            const void* ptr;
+    };    
+    
     template <typename T, GLenum target, GLenum usage>
     class GPUBuffer : public Bindable {
 
@@ -67,6 +110,49 @@ namespace mork {
         // MOstly used with dynamic buffers at initialization
         virtual void setBufferSize(size_t size) {
             glNamedBufferData(bufptr, size, NULL, usage);
+        }
+
+
+
+        // Maps this buffer to a reachable adress space
+        // As noted on the documentation, accessing a buffer in a way incompatible
+        // with the buffer usage may be very slow
+        void* mapBuffer(BufferAccess access) {
+            switch(access) {
+                case(ReadOnly):
+                   return glMapNamedBuffer(bufptr, GL_READ_ONLY);
+                case(WriteOnly):
+                   return glMapNamedBuffer(bufptr, GL_WRITE_ONLY);
+                case(ReadWrite):
+                   return glMapNamedBuffer(bufptr, GL_READ_WRITE);
+                default:
+                   return 0;
+            }
+
+        }
+
+        // Maps this buffer to a reachable adress space
+        // As noted on the documentation, accessing a buffer in a way incompatible
+        // with the buffer usage may be very slow
+        const void* mapBuffer(BufferAccess access) const{
+            switch(access) {
+                case(ReadOnly):
+                    return glMapNamedBuffer(bufptr, GL_READ_ONLY);
+                case(WriteOnly):
+                    mork::warn_logger("Tried to access const buffer with write access..");   
+                    return 0;
+                case(ReadWrite):
+                    mork::warn_logger("Tried to access const buffer with write access..");   
+                    return 0;
+                default:
+                    return 0;
+            }
+
+        }
+
+
+        void unmapBuffer() const {
+            glUnmapNamedBuffer(bufptr);
         }
     private:
 
