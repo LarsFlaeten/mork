d2ea5df8d3aff006edfc0b6fa9690ccc28097af5
LarsFlaeten <lars.flaeten@gmail.com>
2019-03-24 06:57:12 +0100

    Add test for loading shader from file

--- test/testProgram.cpp
+++ test/testProgram.cpp
@@ -107,4 +107,34 @@ TEST_F(ProgramTest, PreprocessShaderTest4)
     ASSERT_EQ(C, 6);
 }
 
+TEST_F(ProgramTest, ShaderFromFileTestErrors)
+{
+    std::ofstream of("/tmp/test.glsl");
+    of << "Line 1\n" << "Line 2\n\nLine4\n";
+    of.close();
 
+    // SHould throw since no _VERTEX_ or _FRAGMENT_
+    ASSERT_THROW(mork::Program prog(330, "/tmp/test.glsl"), std::runtime_error);
+    
+    // SHould throw due to file not found
+    ASSERT_THROW(mork::Program prog(330, "no_such_file.glsl"), std::runtime_error);
+    
+    // Should throw due ot only _VERTEX_, no _FRAGMENT
+    std::ofstream of2("/tmp/test.glsl");
+    of2 << "Line 1\n" << "#ifdef _VERTEX_\nLine 2\n#endif\nLine4\n";
+    of2.close();
+    ASSERT_THROW(mork::Program prog(330, "/tmp/test.glsl"), std::runtime_error);
+ 
+    // Should throw due ot only _FRAGMENT_, no _VERTEX
+    std::ofstream of3("/tmp/test.glsl");
+    of3 << "Line 1\n" << "#ifdef _FRAGMENT_\nLine 2\n#endif\nLine4\n";
+    of3.close();
+    ASSERT_THROW(mork::Program prog(330, "/tmp/test.glsl"), std::runtime_error);
+ 
+}
+
+TEST_F(ProgramTest, ShaderFromFileVSGSFS)
+{
+    mork::Program prog(330, "shaders/normalShader.glsl"); 
+
+}
