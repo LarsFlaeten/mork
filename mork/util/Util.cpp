6e2671be1a4bec43fdea74a986e02bc7c85dbcff
LarsFlaeten <lars.flaeten@gmail.com>
2019-05-02 08:58:04 +0200

    Add utility function to convert csv string to vec3d

--- mork/util/Util.cpp
+++ mork/util/Util.cpp
@@ -21,5 +21,26 @@ namespace mork {
         return str;
     }
 
+    vec3d string2vec3d(const std::string& str) {
+        // Assum comma-separated list of thrww numbers:
+        size_t n = std::count(str.begin(), str.end(), ',');        
+        if(n != 2)
+            throw std::invalid_argument("Expected string with three comma separated values, got [" + str + "]");
+        std::string substr;
+        std::stringstream sstr(str);
+       
+        
+        getline(sstr, substr, ',');
+        double x = std::stod(substr);
+
+        getline(sstr, substr, ',');
+        double y = std::stod(substr);
+
+        getline(sstr, substr, ',');
+        double z = std::stod(substr);
+        return vec3d(x, y, z);
+
+    }
+
 
 }
--- mork/util/Util.h
+++ mork/util/Util.h
@@ -2,11 +2,16 @@
 #define _MORK_UTIL_H_
 
 #include <string>
-
+#include "mork/math/vec3.h"
 
 namespace mork {
 
 std::string random_string( size_t length );
+
+// COnverts a string on the form ##, ##, ## to a vec3d.
+// Will throw exception on any parse error
+vec3d string2vec3d(const std::string& str);
+
 
 
 }
